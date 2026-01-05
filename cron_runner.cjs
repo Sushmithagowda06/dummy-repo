278dd65816020439ca933a9901ee793aeec09697
Sushmitha K <sushmithagowda712@.gmail.com>
2026-01-05 11:07:36 +0530

    Create appointments table on Railway

--- cron_runner.cjs
+++ cron_runner.cjs
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+require("./init_pg.cjs");
 const cron = require("node-cron");
 const exportDb = require("./export_db.cjs");
 const mailer = require("./mailer.cjs");
@@ -21,7 +21,7 @@ async function runJob() {
 }
 
 // 10:15 PM IST
-cron.schedule("31 00 * * *", runJob);
+cron.schedule("15 11 * * *", runJob);
 
 // run immediately (local + Railway)
 runJob();
